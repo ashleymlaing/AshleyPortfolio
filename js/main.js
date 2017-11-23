cf70dd5109e99c04a787a1ac53fb994efce3f575
Ashley Marie Laing <ashleym.laing@gmail.com>
2017-11-23 07:11:29 -0500

    worked on make the resume page have buttons to show extra bullets and going to edit the resume to interchange it with the new one I am creating

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ app.controller('featuredCtrl', function($scope) {
             link: 'carolyn_whelan.html',
             heading:"Carolyn Whelan Portfolio Site",
             type:"Website",
-            paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+            paragraph:"Carolyn Whelan is a writer and I built this site to broadcast her articles. I developed this static site and added Angular 1.0. We separated her articles into catergories, and made them easy to find."
             // "This is a website I built during my time at Resilient Lab. I used HTML, CSS, JavaScript, and Angular 1.0."
         },
         {
@@ -174,3 +174,49 @@ app.controller('featuredCtrl', function($scope) {
       // }
     ]
 })
+
+let closeJobText = "Read More";
+let openJobText = "Show Less";
+
+$("button.microsoft").on("click",function(){
+  $("ul.microsoft .disappear").toggleClass("hiddenBullet");
+  if($("button.microsoft").text()==closeJobText){
+    $("button.microsoft").text(openJobText);
+  }else {
+    $("button.microsoft").text(closeJobText);
+  }
+});
+
+$("button.howard").on("click",function(){
+  $("ul.howard .disappear").toggleClass("hiddenBullet");
+  if($("button.howard").text()==closeJobText){
+    $("button.howard").text(openJobText);
+  }else {
+    $("button.howard").text(closeJobText);
+  }
+});
+
+$("button.resilientDev").on("click",function(){
+  $("ul.resilientDev .disappear").toggleClass("hiddenBullet");
+  if($("button.resilientDev").text()==closeJobText){
+    $("button.resilientDev").text(openJobText);
+  }else {
+    $("button.resilientDev").text(closeJobText);
+  }
+});
+
+$("button.resilientLab").on("click",function(){
+  $("ul.resilientLab .disappear").toggleClass("hiddenBullet");
+  if($("button.resilientLab").text()==closeJobText){
+    $("button.resilientLab").text(openJobText);
+  }else {
+    $("button.resilientLab").text(closeJobText);
+  }
+});
+
+let project_cw = [];
+let project_lg = [];
+let project_rvms = [];
+let project_quilt = [];
+let project_weather = [];
+let project_todo = [];
