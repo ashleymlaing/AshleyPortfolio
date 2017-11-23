fbfcd466d9793854050e6d8d9bf693c768d3bedd
Ashley Marie Laing <ashleym.laing@gmail.com>
2017-11-23 08:19:29 -0500

    changed to go down to 4 projects and plan to add a section for links to free time/practice projects

--- js/main.js
+++ js/main.js
@@ -29,15 +29,15 @@ app.controller('featuredCtrl', function($scope) {
             type:"WordPress Theme",
             paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         },
-        {
-            projectName: 'To Do List',
-            frontPage: true,
-            featuredImage: 'img/project_quilt/quilt-tablet-mock.png',
-            link: 'toDoList.html',
-            heading:"To Do List Application",
-            type:"JavaScript Application",
-            paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        },
+        // {
+        //     projectName: 'To Do List',
+        //     frontPage: true,
+        //     featuredImage: 'img/project_quilt/quilt-tablet-mock.png',
+        //     link: 'toDoList.html',
+        //     heading:"To Do List Application",
+        //     type:"JavaScript Application",
+        //     paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        // },
         {
             projectName: 'Level Ground MMA',
             frontPage: true,
@@ -47,15 +47,15 @@ app.controller('featuredCtrl', function($scope) {
             type:"Jekyll Theme",
             paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
         },
-        {
-            projectName: 'Boston Weather App',
-            frontPage: true,
-            featuredImage: 'img/project_quilt/quilt-tablet-mock.png',
-            link: 'weather.html',
-            heading:"Boston Weather Application",
-            type:"JavaScript API Application",
-            paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        },
+        // {
+        //     projectName: 'Boston Weather App',
+        //     frontPage: true,
+        //     featuredImage: 'img/project_quilt/quilt-tablet-mock.png',
+        //     link: 'weather.html',
+        //     heading:"Boston Weather Application",
+        //     type:"JavaScript API Application",
+        //     paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        // },
         {
         project:"Carolyn Whelan",
         image:"img/project_cw/cw_featured.png",
@@ -128,42 +128,42 @@ app.controller('featuredCtrl', function($scope) {
   sectionHeading:"Why I Did It",
   writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
   },
-  {
-  project:"To Do List",
-  image:"img/project_quilt/quilt-tablet-mock.png",
-  sectionHeading:"What I Created",
-  writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-  project:"To Do List",
-  image:"img/project_quilt/quilt-tablet-mock.png",
-  sectionHeading:"What I Used and Learned",
-  writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-  project:"To Do List",
-  image:"img/project_quilt/quilt-tablet-mock.png",
-  sectionHeading:"Why I Did It",
-  writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-  project:"Weather",
-  image:"img/project_quilt/quilt-tablet-mock.png",
-  sectionHeading:"What I Created",
-  writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-  project:"Weather",
-  image:"img/project_quilt/quilt-tablet-mock.png",
-  sectionHeading:"What I Used and Learned",
-  writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-  project:"Weather",
-  image:"img/project_quilt/quilt-tablet-mock.png",
-  sectionHeading:"Why I Did It",
-  writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
+  // {
+  // project:"To Do List",
+  // image:"img/project_quilt/quilt-tablet-mock.png",
+  // sectionHeading:"What I Created",
+  // writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  // },
+  // {
+  // project:"To Do List",
+  // image:"img/project_quilt/quilt-tablet-mock.png",
+  // sectionHeading:"What I Used and Learned",
+  // writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  // },
+  // {
+  // project:"To Do List",
+  // image:"img/project_quilt/quilt-tablet-mock.png",
+  // sectionHeading:"Why I Did It",
+  // writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  // },
+  // {
+  // project:"Weather",
+  // image:"img/project_quilt/quilt-tablet-mock.png",
+  // sectionHeading:"What I Created",
+  // writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  // },
+  // {
+  // project:"Weather",
+  // image:"img/project_quilt/quilt-tablet-mock.png",
+  // sectionHeading:"What I Used and Learned",
+  // writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  // },
+  // {
+  // project:"Weather",
+  // image:"img/project_quilt/quilt-tablet-mock.png",
+  // sectionHeading:"Why I Did It",
+  // writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  // },
 
 
       // {
