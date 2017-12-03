d6dcafc3c22e2beffce4b4ef53b96308990bf0dd
Ashley Marie Laing <ashleym.laing@gmail.com>
2017-12-03 18:43:44 -0500

    made a change in js file

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@ app.controller('featuredCtrl', function($scope) {
   project:"Level Ground",
   image:"img/project_lg/lg3.png",
   sectionHeading:"Why I Did It",
-  writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  writeUp:"I did this project to learn Jekyll and I loved the Level Ground MMA concept."
   },
   {
   project:"Quilt",
