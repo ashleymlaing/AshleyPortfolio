57244ff1dee65fe6a98bebc39753af06ab6adde3
Ashley Marie Laing <ashleym.laing@gmail.com>
2017-12-04 20:02:56 -0500

    Added some text in main.js for a project

--- js/main.js
+++ js/main.js
@@ -126,7 +126,7 @@ app.controller('featuredCtrl', function($scope) {
   project:"Quilt",
   image:"img/project_quilt/quilt-in-page3.png",
   sectionHeading:"Why I Did It",
-  writeUp:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  writeUp:"This project was a fun project that helped me see some Ruby on Rails, and use BootStrap. The Second half of this project helped me enjoy to learn SASS."
   },
   // {
   // project:"To Do List",
